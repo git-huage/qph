09459dea62ac1a757505bda674c07c7218ac4d62
qph <651024151@qq.com>
2020-03-23 19:45:52 +0800

    第一次提交

---
+++ src/main/java/com/woniu/test/tset.java
@@ -0,0 +1,24 @@
+package com.woniu.test;
+
+/**
+ * @PACKAGE_NAME: com.woniu.test
+ * @NAME: tset
+ * @USER: huage
+ * @DATE: 2020/3/23
+ * @TIME: 19:39
+ * @YEAR: 2020
+ * @MONTH: 03
+ * @MONTH_NAME_SHORT: 3月
+ * @MONTH_NAME_FULL: 三月
+ * @DAY: 23
+ * @DAY_NAME_SHORT: 周一
+ * @DAY_NAME_FULL: 星期一
+ * @HOUR: 19
+ * @MINUTE: 39
+ * @PROJECT_NAME: ideaTest
+ **/
+public class tset {
+    public static void main(String[] args) {
+        System.out.println("huagege");
+    }
+}
